a9428f81080462b184225db6645330d6d881247b
Warden <vovatkach2412@gmail.com>
2026-05-25 14:48:51 +0300

    Embedder remake

--- rag_wv/embedding/embedding.py
+++ rag_wv/embedding/embedding.py
@@ -2,17 +2,16 @@ from dotenv import load_dotenv
 load_dotenv()
 from FlagEmbedding import BGEM3FlagModel
 from typing import List
-from ..config import EMB_MODEL_NAME, DEFAULT_BATCH_SIZE
+from ..config import EMB_MODEL_NAME, DEFAULT_BATCH_SIZE, SAVE_HYBRID
 
 class Embedder:
-    def __init__(self, use_hybrid: bool = False) -> None:
-
-        self.model: BGEM3FlagModel = BGEM3FlagModel(model_name_or_path=EMB_MODEL_NAME, use_fp16=True, batch_size=DEFAULT_BATCH_SIZE, return_dense=True, return_sparse=use_hybrid, trust_remote_code=True) #добавить batch_size в конфиг
-        self.use_hybrid: bool = use_hybrid
+    def __init__(self) -> None:
+        self.model: BGEM3FlagModel = BGEM3FlagModel(model_name_or_path=EMB_MODEL_NAME, use_fp16=True, batch_size=DEFAULT_BATCH_SIZE, return_dense=True, return_sparse=SAVE_HYBRID, trust_remote_code=True) #добавить batch_size в конфиг
+        self.save_hybrid: bool = SAVE_HYBRID
 
     def embed(self, chunks: List[str]):
         try:
-            result = self.model.encode(chunks, batch_size=DEFAULT_BATCH_SIZE, return_dense=True, return_sparse=self.use_hybrid)
+            result = self.model.encode(chunks, batch_size=DEFAULT_BATCH_SIZE, return_dense=True, return_sparse=self.save_hybrid)
             return result
         except Exception as e:
             pass
