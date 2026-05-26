ef0d28b20f4ac82b78a52fe7f645189b193c80eb
Warden <vovatkach2412@gmail.com>
2026-05-26 17:20:44 +0300

    Architecture replaning

--- rag_wv/pipeline/pipeline.py
+++ rag_wv/pipeline/pipeline.py
@@ -1,73 +1,10 @@
-from typing import List, Dict, Any
-from pathlib import Path, PurePath
+import uuid
+from typing import List
 from hashlib import file_digest
 from models import Chunk
-import pymupdf4llm
-import re
-import uuid
+from pathlib import Path
+from ..utils import data_normalize
 
-def extract_author(text: str) -> List[str]:
-    pattern = r'[А-ЯЁ][а-яё]+\s[А-ЯЁ]\.[А-ЯЁ]\.|[А-ЯЁ]\.[А-ЯЁ]\.\s[А-ЯЁ][а-яё]+'
-    authors = set(re.findall(pattern, text))
-    normalized = set()
-    for author in authors:
-        parts = author.split()
-        if parts[0].endswith('.'):
-            normalized.add(f"{parts[1]} {parts[0]}")
-        else:
-            normalized.add(f"{parts[0]} {parts[1]}")
-
-    return list(normalized)
-
-def clean_pdf_text(text: str) -> str:
-    text = re.sub(r"==> picture \[\d+ x \d+\] <==", "\n[ИЗОБРАЖЕНИЕ]\n", text)
-
-    text = re.sub(r"----- Start of picture text -----", "", text)
-    text = re.sub(r"----- End of picture text -----", "", text)
-
-    text = re.sub(r"<br>", "", text)
-    
-    return text.strip()
-
-def get_doc_hash(document) -> str:
-    with open(document, 'rb') as f:
-        digest = file_digest(f, "sha256")
-    return digest.hexdigest()
-
-def data_normalize(document) -> Dict[str, Any]:    
-    try:
-        pages = pymupdf4llm.to_text(document, ocr_language = "rus+eng", page_chunks = True)
-        print(pages)
-        pages_data = [
-            {
-                "page": page["metadata"]["page_number"], 
-                "text": clean_pdf_text(page["text"]),       
-            } 
-            for page in pages
-        ]
-
-        source = PurePath(document).name
-        
-        author_pages = [0, 1, -1, -2] if len(pages_data) > 4 else [0, -1]
-        author_mentioned_pages = " ".join(pages_data[page]["text"] for page in author_pages)
-
-        author = extract_author(author_mentioned_pages)
-
-        d_hash = get_doc_hash(document)
-
-        data = {
-            "payload": {
-                "source": source,
-                "author": author,
-                "pages": pages_data,
-                "doc_hash": d_hash,
-            },
-        }
-
-        return data
-    except Exception as e:
-        print(e)
-        raise
 
 class Pipeline:
     def __init__(self, chunker, embedder, vector_db) -> None:
